34c53497703b0d0b3528d51c8859c9d96beeda30
yaniferhaoui <yani.ferhaoui@hotmail.fr>
2024-09-12 18:16:50 +0200

    viem/account-abstraction/types/entryPointVersion is not a valid entrypoint.

--- src/call-libraries.ts
+++ src/call-libraries.ts
@@ -1,5 +1,4 @@
 import { constants } from '@zerodev/sdk';
 import { ENTRYPOINT_ADDRESS_V07, getEntryPointVersion } from 'permissionless';
-import type { EntryPointVersion } from 'viem/account-abstraction/types/entryPointVersion';
 
 // Just some imports to trigger the compilation errors
