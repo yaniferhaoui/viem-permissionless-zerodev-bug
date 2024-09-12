1d8e9018dedbfb03a312a00fc2afcfddaee9af38
yaniferhaoui <yani.ferhaoui@hotmail.fr>
2024-09-12 19:24:22 +0200

    specify version

--- src/call-libraries.ts
+++ src/call-libraries.ts
@@ -1,4 +1,5 @@
 import { constants } from '@zerodev/sdk';
 import { ENTRYPOINT_ADDRESS_V07, getEntryPointVersion } from 'permissionless';
+//import type { EntryPointVersion } from 'viem/account-abstraction/types/entryPointVersion';
 
 // Just some imports to trigger the compilation errors
